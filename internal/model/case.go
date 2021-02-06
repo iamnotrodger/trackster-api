e8f124273f92980c757bda71cf06cd5a41cbf8bc
iamnotrodger <rodger.retanal24@gmail.com>
2021-02-06 18:07:42 -0500

    modified insert trace

--- internal/model/case.go
+++ internal/model/case.go
@@ -1,7 +1,10 @@
 package model
 
+import "time"
+
 // Case struct
 type Case struct {
-	CaseID string `json:"case_id"`
-	UserID string `json:"user_id"`
+	CaseID         string    `json:"case_id"`
+	UserID         string    `json:"user_id"`
+	TimeOfSymptoms time.Time `json:"time_of_symptoms"`
 }
--- internal/model/trace.go
+++ internal/model/trace.go
@@ -4,18 +4,17 @@ import "github.com/jmoiron/sqlx"
 
 // Trace struct
 type Trace struct {
-	TraceID string `json:"trace_id"`
+	TraceID   string `json:"trace_id"`
 	ContactID string `json:"contact_id"`
+	UserID    string `json:"user_id"`
 }
 
+//Insert Trace
 func (this *Trace) Insert(db *sqlx.DB) error {
-	sqlStatement := "INSERT INTO trace (contact_id) VALUES ($1) RETURNING trace_id;"
+	sqlStatement := "INSERT INTO trace (contact_id) VALUES ($1, $2) RETURNING trace_id;"
 	var traceID string
 	err := db.QueryRow(sqlStatement, this.ContactID).Scan(&traceID)
-	if err != nil {
-		return err
-	} else {
-		this.TraceID = traceID
-		return err
-	}
+
+	this.TraceID = traceID
+	return err
 }
