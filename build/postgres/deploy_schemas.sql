8e34bb08f119bb44948ba5b95bee3c971f38c6c2
iamnotrodger <rodger.retanal24@gmail.com>
2021-02-06 18:01:37 -0500

    Added database seeds

--- build/postgres/deploy_schemas.sql
+++ build/postgres/deploy_schemas.sql
@@ -3,3 +3,6 @@
 \i '/docker-entrypoint-initdb.d/tables/Contact.sql' 
 \i '/docker-entrypoint-initdb.d/tables/Cases.sql' 
 \i '/docker-entrypoint-initdb.d/tables/Trace.sql' 
+
+-- Seeds
+\i '/docker-entrypoint-initdb.d/seeds/seeds.sql' 
